1a841ba6e47abb718ffd12357529bfb44e159142
Frederick-S <mao_xiaodan@hotmail.com>
2022-01-24 22:00:32 +0800

    welcome

--- src/components/Repos.vue
+++ src/components/Repos.vue
@@ -3,9 +3,11 @@
     <div class="container column is-half">
       <b-navbar>
         <template #end>
-          <b-navbar-item href="#">
-            Welcome, {{ me.name }}
-          </b-navbar-item>
+          <b-navbar-dropdown :label="welcome">
+            <b-navbar-item href="#">
+              Logout
+            </b-navbar-item>
+          </b-navbar-dropdown>
         </template>
       </b-navbar>
     </div>  
@@ -14,6 +16,11 @@
 
 <script>
 export default {
+  computed: {
+    welcome() {
+      return `Welcome, ${this.me.name}`
+    }
+  },
   data() {
     return {
       me: JSON.parse(localStorage.getItem('yuque-me'))
